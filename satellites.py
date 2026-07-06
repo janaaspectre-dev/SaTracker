d5c1e04287217061b7d06f1b9584df6d831aa301
janaaspectre-dev <janaaspectre@gmail.com>
2026-07-06 11:13:35 +0530

    Add files via upload

---
+++ satellites.py
@@ -0,0 +1,145 @@
+satellites = {
+
+    "ISS": {
+        "norad": "25544",
+        "country": "USA / Russia",
+        "launch": "1998",
+        "status": "Active",
+        "latitude": 12.45,
+        "longitude": 78.90,
+        "altitude": "420 km",
+        "velocity": "7.66 km/s",
+
+
+    },
+
+    "HUBBLE": {
+        "norad": "20580",
+        "country": "USA",
+        "launch": "1990",
+        "status": "Active",
+        "latitude": 22.10,
+        "longitude": 45.30,
+        "altitude": "540 km",
+        "velocity": "7.50 km/s",
+
+
+    },
+
+    "TIANGONG": {
+        "norad": "48274",
+        "country": "China",
+        "launch": "2021",
+        "status": "Active",
+        "latitude": 0,
+        "longitude": 0,
+        "altitude": "---",
+        "velocity": "---"
+
+    },
+
+    "LANDSAT-8": {
+        "norad": "39084",
+        "country": "USA",
+        "launch": "2013",
+        "status": "Active",
+        "latitude": 0,
+        "longitude": 0,
+        "altitude": "---",
+        "velocity": "---"
+
+   
+    },
+
+    "NOAA-20": {
+        "norad": "43013",
+        "country": "USA",
+        "launch": "2017",
+        "status": "Active",
+        "latitude": 0,
+        "longitude": 0,
+        "altitude": "---",
+        "velocity": "---"
+
+    },
+    "CHANDRA": {
+        "norad": "25867",
+        "country": "USA",
+        "launch": "1999",
+        "status": "Active",
+        "latitude": 0,
+        "longitude": 0,
+        "altitude": "---",
+        "velocity": "---"
+
+    
+    },
+
+    "XMM-NEWTON": {
+        "norad": "25989",
+        "country": "ESA",
+        "launch": "1999",
+        "status": "Active",
+        "latitude": 0,
+        "longitude": 0,
+        "altitude": "---",
+        "velocity": "---"
+    },
+
+    "GAIA": {
+        "norad": "39129",
+        "country": "ESA",
+        "launch": "2013",
+        "status": "Active",
+        "latitude": 0.0,
+        "longitude": 0.0,
+        "altitude": "---",
+        "velocity": "---"
+    },
+
+    "SES-17": {
+        "norad": "49332",
+        "country": "Luxembourg",
+        "launch": "2021",
+        "status": "Active",
+        "latitude": 0.0,
+        "longitude": 0.0,
+        "altitude": "---",
+        "velocity": "---"
+    },
+
+    "STARLINK-1008": {
+        "norad": "44714",
+        "country": "USA",
+        "launch": "2019",
+        "status": "Active",
+        "latitude": 0.0,
+        "longitude": 0.0,
+        "altitude": "---",
+        "velocity": "---"
+
+    },
+
+    "SENTINEL-2A": {
+        "norad": "40697",
+        "country": "ESA",
+        "launch": "2015",
+        "status": "Active",
+        "latitude": 0.0,
+        "longitude": 0.0,
+        "altitude": "---",
+        "velocity": "---"
+    },
+
+    "TERRA": {
+        "norad": "25994",
+        "country": "USA",
+        "launch": "1999",
+        "status": "Active",
+        "latitude": 0.0,
+        "longitude": 0.0,
+        "altitude": "---",
+        "velocity": "---"
+}}
+
+
